492a5537975751c3893277dae02ef04f523491f0
Connor Chase <cchase4210@gmail.com>
2024-10-24 14:00:02 -0700

    modified CMake.txt

--- main.cpp
+++ main.cpp
@@ -9,11 +9,8 @@ int main(void)
     queueOBJ.enqueue(5);
 
     std::cout << queueOBJ.isEmpty() << std::endl;
-
     std::cout << queueOBJ.contains(5) << std::endl;
-
     std::cout << queueOBJ.contains(7) << std::endl;
-
     std::cout << queueOBJ.dequeue() << std::endl;
 
     return 0;
