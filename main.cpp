6bd08690117cf888235c86cd54d39906dd4c1251
Ethan <strider3039@gmail.com>
2024-11-17 16:28:13 -0800

    Created Main Menu

--- main.cpp
+++ main.cpp
@@ -1,6 +1,8 @@
 #include "Avl.hpp"
 #include "Stack.hpp"
 #include "menuItems.hpp"
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Texture.hpp>
 #include <utility>
 
 int main(void)
@@ -23,29 +25,15 @@ int main(void)
 
 
     sf::Font font;
-
     if (!font.loadFromFile("arial.ttf"))
     {
         std::cout << "Failed to load font" << std::endl;
     }
 
-    // Load PNG background
+    sf::Sprite background;
     sf::Texture backgroundTexture;
-    if (!backgroundTexture.loadFromFile("AV Background.png")) {
-        std::cerr << "Failed to load background image" << std::endl;
-        return -1;
-    }
-
-    sf::Sprite backgroundSprite;
-    backgroundSprite.setTexture(backgroundTexture);
-
-    // Scale background to fit the window
-    sf::Vector2u textureSize = backgroundTexture.getSize();
-    sf::Vector2u windowSize = window.getSize();
-    // backgroundSprite.setScale(
-    //     screenWidth / textureSize.x,
-    //     screenHeight / textureSize.y
-    // );
+    sf::Text title;
+    mainMenuScreen(background, backgroundTexture, title, font, window);
     
     vector<Button> buttons;
     mainMenuItems(buttons, font, screenWidth, screenHeight);
@@ -66,7 +54,9 @@ int main(void)
             }
         }
 
-        window.draw(backgroundSprite);
+        window.draw(background);
+        window.draw(background);
+        window.draw(title);
 
         for (auto& buttonItr : buttons)
         {
--- menuItems.hpp
+++ menuItems.hpp
@@ -1,14 +1,44 @@
 #include "newButton.hpp"
+#include <SFML/Graphics/Color.hpp>
 #include <SFML/Graphics/Font.hpp>
+#include <SFML/Graphics/Sprite.hpp>
+#include <SFML/Graphics/Text.hpp>
+#include <SFML/Graphics/Texture.hpp>
 
+
+inline void mainMenuScreen(sf::Sprite& backgroundSprite, sf::Texture& backgroundTexture, sf::Text& title, sf::Font& font, sf::Window& window)
+{
+    
+    // Create Menu Title
+    title = sf::Text("Algorithm Visualizer", font, 70);
+    title.setStyle(sf::Text::Underlined | sf::Text::Bold);
+    title.setFillColor(sf::Color::Cyan);
+
+    // Center the title
+    sf::Vector2u windowSize = window.getSize();
+    title.setPosition(
+        windowSize.x / 2.f - title.getLocalBounds().width / 2.f,
+        5*windowSize.y / 20.f // Place it near the top
+    );
+
+    if (!backgroundTexture.loadFromFile("AV Background.png")) {
+        std::cerr << "Failed to load background image" << std::endl;
+    }
+
+    backgroundSprite.setTexture(backgroundTexture);
+
+    // Scale background to fit the window
+    sf::Vector2u textureSize = backgroundTexture.getSize();
+    windowSize = window.getSize();
+}
 
 inline void mainMenuItems(vector<Button>& buttons, sf::Font& font, double width, double height)
 {
-    Button list_button("Linked List", font, sf::Vector2f(width / 2, 3*height / 10));
-    Button queue_button("Queue", font, sf::Vector2f(width / 2, 4*height / 10));
-    Button stack_button("Stack", font, sf::Vector2f(width / 2, 5*height / 10));
-    Button bst_button("Binary Tree", font, sf::Vector2f(width / 2, 6*height / 10));
-    Button avl_button("AVL Tree", font, sf::Vector2f(width / 2, 7*height / 10));
+    Button list_button("Linked List", font, sf::Vector2f(width / 2, 7*height / 15));
+    Button queue_button("Queue", font, sf::Vector2f(width / 2, 8*height / 15));
+    Button stack_button("Stack", font, sf::Vector2f(width / 2, 9*height / 15));
+    Button bst_button("Binary Tree", font, sf::Vector2f(width / 2, 10*height / 15));
+    Button avl_button("AVL Tree", font, sf::Vector2f(width / 2, 11*height / 15));
 
     buttons.push_back(list_button);
     buttons.push_back(queue_button);
