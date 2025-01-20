d3158a680058d4a57cbd2060cad39fe5f41152ae
Ethan <strider3039@gmail.com>
2025-01-19 21:12:14 -0800

    test avl values

--- AVLWindow.hpp
+++ AVLWindow.hpp
@@ -30,17 +30,18 @@ protected:
         switch (key)
         {
         case sf::Keyboard::Num1: // Insert test values
-            this->tree->insert(20, this->mFont);
-            this->tree->insert(50, this->mFont);
-            this->tree->insert(30, this->mFont);
-            this->tree->insert(70, this->mFont);
-            this->tree->insert(40, this->mFont);
-            this->tree->insert(60, this->mFont);
-            this->tree->insert(80, this->mFont);
             this->tree->insert(10, this->mFont);
+            this->tree->insert(20, this->mFont);
             this->tree->insert(5, this->mFont);
             this->tree->insert(15, this->mFont);
             this->tree->insert(25, this->mFont);
+            this->tree->insert(30, this->mFont);
+            this->tree->insert(40, this->mFont);
+            this->tree->insert(50, this->mFont);
+            this->tree->insert(60, this->mFont);
+            this->tree->insert(70, this->mFont);
+            this->tree->insert(80, this->mFont);
+            
             break;
         case sf::Keyboard::R: // Reset the AVL tree
             this->tree->resetHelper(this->tree->root);
