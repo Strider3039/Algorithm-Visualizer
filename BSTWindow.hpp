640d4c39bb88b96f911503b662ee5f2937b60f55
Ethan <strider3039@gmail.com>
2025-01-19 21:12:53 -0800

    update handlekeypress for bst

--- BSTWindow.hpp
+++ BSTWindow.hpp
@@ -22,12 +22,32 @@ private:
     {
     }
 
-    void handleKeyPress(sf::Keyboard::Key key, sf::RenderWindow& window) override 
+    void handleKeyPress(sf::Keyboard::Key key, sf::RenderWindow& window) override
     {
-
-        if (key == sf::Keyboard::R) 
+        switch (key)
         {
+        case sf::Keyboard::Num1: // Insert test values
+            this->tree->insert(10, this->mFont);
+            this->tree->insert(20, this->mFont);
+            this->tree->insert(5, this->mFont);
+            this->tree->insert(15, this->mFont);
+            this->tree->insert(25, this->mFont);
+            this->tree->insert(30, this->mFont);
+            this->tree->insert(40, this->mFont);
+            this->tree->insert(50, this->mFont);
+            this->tree->insert(60, this->mFont);
+            this->tree->insert(70, this->mFont);
+            this->tree->insert(80, this->mFont);
+            
+            break;
+        case sf::Keyboard::R: // Reset the AVL tree
             this->tree->resetHelper(this->tree->root);
+            break;
+        case sf::Keyboard::Escape: // Close the window
+            window.close();
+            break;
+        default:
+            break;
         }
     }
 };
